845684c6c6d229db1703ed881b97d93ef1496fd5
Jose Vasquez <jose.vasquez@fracttal.com>
2018-04-04 17:53:00 -0500

    initial commit

---
+++ sopa.py
@@ -0,0 +1,24 @@
+column0=["A","B","C"]
+column1=["D","E","F"]
+column2=["G","H","I"]
+column3=["J","K","L"]
+soup=[column0,column1,column2,column3]
+
+x=0
+y=0
+
+listStrings = []
+#add columns
+for i in range(len(soup)):
+    listStrings.append(''.join(soup[i]))
+
+print(listStrings)
+#add rows
+
+for i in range(len(soup)):
+    column = soup[i]
+    
+    currentArray = []
+    for j in range(len(column)):
+        currentArray.append(soup[i][j])
+    print(''.join(currentArray))
