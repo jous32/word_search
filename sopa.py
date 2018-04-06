d1f2fddf4b6494bc5d56aa7a9a44b1b13f9575f6
Jose Vasquez <jose.vasquez@fracttal.com>
2018-04-06 10:34:44 -0500

    add rows and reverse strings

--- sopa.py
+++ sopa.py
@@ -1,24 +1,31 @@
+#import pdb
+#pdb.set_trace()
 column0=["A","B","C"]
 column1=["D","E","F"]
 column2=["G","H","I"]
 column3=["J","K","L"]
 soup=[column0,column1,column2,column3]
-#comment
-x=0
-y=0
 
 listStrings = []
 #add columns
+print('0 for')
 for i in range(len(soup)):
     listStrings.append(''.join(soup[i]))
 
-print(listStrings)
 #add rows
-
-for i in range(len(soup)):
-    column = soup[i]
-    
+for j in range(len(soup[0])):
     currentArray = []
-    for j in range(len(column)):
+    for i in range(len(soup)):
         currentArray.append(soup[i][j])
-    print(''.join(currentArray))
+    listStrings.append(''.join(currentArray))
+
+# reverse strings
+listStringsReversed = []
+for s in listStrings:
+    listStringsReversed.append(s[::-1])
+
+#add reversed strings
+for rs in listStringsReversed:
+    listStrings.append(rs)
+
+print(listStrings)
